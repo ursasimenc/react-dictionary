002eb424afec05264f04312ea7eb72a13b4700aa
ursasimenc <ursasimenc17@gmail.com>
2023-06-12 18:59:17 +0200

    minor fixes

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,7 +1,7 @@
 const Error = (props) => {
 	return (
 		<div className="flex flex-col items-center gap-4">
-			<h2>{props.data.title}</h2>
+			<h1>{props.data.title}</h1>
 			<p className="text-gray_5">{props.data.message}</p>
 		</div>
 	);
